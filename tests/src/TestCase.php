4c73236cc5eb1b18d4b10e073dbbfc6cab0a683d
Pavel Buchnev <butschster@gmail.com>
2022-11-29 14:17:02 +0400

    Initial commit

---
+++ tests/src/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VendorName\Skeleton\Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    public function rootDirectory(): string
+    {
+        return __DIR__.'/../';
+    }
+}
